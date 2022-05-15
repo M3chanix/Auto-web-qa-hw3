5b9ff08480efddb32b7a2670cc7177ff644ab67a
M3chanix <M3chanix@protonmail.com>
2022-05-15 03:22:42 +0300

    json crud methods and improvements

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,26 +1,41 @@
-import requests
+from tests.request_methods import post_request, patch_request, \
+        get_request, delete_request
 import pytest
 from hw3.Post import Post
-from hw3.Comment import Comment
 
 
-# Поддерживаются все основные методы по работе с ресурсами - post, get, patch, delete
-# можно организовать фикстуры с предварительным созданием тестовых сущностей
 @pytest.fixture
 def create_post():
-    post_dict = {'title': 'a new post title', 'body': 'new post spawned', 'userId': 1}
+    post_dict = {'id': 46, 'title': 'aut quo modi neque nostrum ducimus',
+            'body': 'voluptatem quisquam iste\nvoluptatibus natus officiis facilis dolorem\nquis quas ipsam\nvel et voluptatum in aliquid',
+            'userId': 5}
     post = Post(**post_dict)
     return post
 
-def send_post(post):
+def test_post_created(create_post):
     url = 'https://jsonplaceholder.typicode.com/posts'
-    payload = post.dict()
-    r = requests.post(url, params=payload)
+    payload = create_post.dict(exclude={'id'})
+    returned_post = post_request(url, payload)
+    assert returned_post.status_code == 201
 
-def get_post(post):
-    url = 'https://jsonplaceholder.typicode.com/posts/{}'.format(post.id)
-    r = requests.get(url)
-    my_post = post.dict()
-    portal_post = r.json()
-    return(my_post, portal_post)
+def test_post_updated(create_post):
+    create_post.body = 'new post body'
+    url = 'https://jsonplaceholder.typicode.com/posts/{}'.format(create_post.id)
+    payload = create_post.dict(exclude={'id'})
+    returned_post = patch_request(url, payload)
+    assert returned_post.status_code == 200
 
+def test_post_getted(create_post):
+    url = 'https://jsonplaceholder.typicode.com/posts/{}'.format(create_post.id)
+    returned_post = get_request(url).json()
+    created_post = create_post.dict()
+    for key in created_post:
+        assert created_post[key] == returned_post[key]
+
+def test_post_deleted(create_post):
+    url = 'https://jsonplaceholder.typicode.com/posts/{}'.format(create_post.id)
+    returned_post = delete_request(url)
+    assert returned_post.status_code == 200
+
+
+# Написать тесты с параметризацией
